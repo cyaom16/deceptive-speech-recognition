50df466142ee7e9c3ae1675695648db5f5dafe7c
Chen Yao <cyaom16@gmail.com>
2018-04-10 17:34:51 -0400

    Update docstring

--- util.py
+++ util.py
@@ -1,21 +1,33 @@
 import numpy as np
 
 
-def read_data(data_dir, suffix='_'):
+def load_data(data_dir, suffix='_'):
     data = np.load(data_dir)
     return data['X' + suffix], data['y' + suffix], data['l' + suffix]
 
 
 def make_batches(*arrays, batch_size=1, shuffle=True):
+    """
+    Batch maker
+
+    Args:
+        array_1, array_2, ... : array_like
+        batch_size (int): batch size
+        shuffle (bool, optional): True for shuffle, False otherwise
+
+    Returns:
+        A list of array batches
+
+    """
     arrays = list(arrays)
     num_samples = arrays[0].shape[0]
 
     if shuffle:
         s = np.arange(num_samples)
         np.random.shuffle(s)
-        arrays = [arr[s] for arr in arrays]
+        arrays = [a[s] for a in arrays]
 
-    return [np.array_split(arr, num_samples // batch_size) for arr in arrays]
+    return [np.array_split(a, num_samples // batch_size) for a in arrays]
 
 
 def resample(inputs, seq_lens, tgt_len):
@@ -23,12 +35,13 @@ def resample(inputs, seq_lens, tgt_len):
     Splice input sequences into sub-sequences with a given sample length
 
     Args:
-        inputs:   NumPy array. Raw variable-length sequences
-        seq_lens: NumPy array. Sequence lengths
-        tgt_len:  int. Target sequence length
+        inputs (NumPy array):   Raw variable-length sequences
+        seq_lens (NumPy array): Sequence lengths
+        tgt_len (int):          Target sequence length
 
     Returns:
-        resampled: list. Variable-length sub-sequences
+        resampled (list): Variable-length sub-sequences
+
     """
     threshold_index = seq_lens > tgt_len
     resampled = []
@@ -42,18 +55,18 @@ def resample(inputs, seq_lens, tgt_len):
 
 def pad_sequences(inputs, seq_lens, max_len=None):
     """
-    This function performs zero-padding on the given sequences to their maximum lengths
+    Performs zero-padding on the given sequences to the maximum length
 
     Args:
-        inputs:   NumPy array. Raw variable-length sequences
-        seq_lens: NumPy array. Sequence lengths
-        max_len:  int. Max sequence length
+        inputs (NumPy array):    Raw variable-length sequences
+        seq_lens (NumPy array):  Sequence lengths
+        max_len (int, optional): Max sequence length
 
     Returns:
-        inputs: list. A list of padded sequences
+        inputs (NumPy array): Padded sequences
+
     """
     max_len = max(seq_lens) if max_len is None else max_len
     for i, x in enumerate(inputs):
         inputs[i] = np.pad(x, ((0, max_len - seq_lens[i]), (0, 0)), 'constant')
-
     return inputs
